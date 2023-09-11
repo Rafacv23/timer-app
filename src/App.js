6f26992dafd3efddb495b250f4edf844c0b848f5
Rafa Canosa <rafacv23@gmail.com>
2023-09-12 01:05:54 +0200

    Added styles folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import "./App.css"
+import "./styles/App.css"
 import { Routes, Route } from "react-router-dom"
 import Clock from "./containers/clock"
 import BtnPanel from "./containers/btn-panel"
